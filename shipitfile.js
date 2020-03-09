3f51269860ef6fce8fe4646651b1a2ba1f1f3315
レンナルト・ グランストロム <l_granstrom@intra.local>
2020-03-09 10:20:12 +0900

    shared

--- shipitfile.js
+++ shipitfile.js
@@ -15,7 +15,8 @@ module.exports = shipit => {
       deployTo: '/home/deploy/tracker',
       repositoryUrl: 'https://github.com/lentmiien/GCSTracker.git',
       shared: {
-        overwrite: true
+        overwrite: true,
+        dirs: ['../shared']
       }
     },
     staging: {
