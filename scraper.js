b5e49a3ed96e429c653840ea7173f79544dbab20
lentmiien <lentmiien@gmail.com>
2020-03-10 22:25:31 +0900

    zxios then/catch, fixes #6

--- scraper.js
+++ scraper.js
@@ -5,10 +5,29 @@ let HTML_status;
 let HTML_statusText;
 
 const fetchData = async siteUrl => {
-  const result = await axios.get(siteUrl);
-  HTML_status = result.status;
-  HTML_statusText = result.statusText;
-  return cheerio.load(result.data);
+  let data;
+  await axios
+    .get(siteUrl)
+    .then(response => {
+      HTML_status = response.status;
+      HTML_statusText = response.statusText;
+      data = cheerio.load(response.data);
+    })
+    .catch(error => {
+      if (error.response == undefined) {
+        HTML_status = error.errno;
+        HTML_statusText = error.errno;
+      } else {
+        HTML_status = error.response.status;
+        HTML_statusText = error.response.statusText;
+      }
+      data = undefined;
+    });
+  return data;
+  // const result = await axios.get(siteUrl);
+  // HTML_status = result.status;
+  // HTML_statusText = result.statusText;
+  // return cheerio.load(result.data);
 };
 
 const getResults = async (siteUrl, carrier) => {
@@ -18,55 +37,57 @@ const getResults = async (siteUrl, carrier) => {
     HTML_statusText: HTML_statusText
   };
 
-  if (carrier == 'JP') {
-    const tracking_data = [];
-    // Japan Post scrapping
-    let date_index = -9;
-    let date = '';
-    let status = '';
-    $('td').each((index, element) => {
-      const content = $(element).text();
-      if (content.indexOf('/') == 4) {
-        date_index = index;
-        date = content
-          .split(' ')
-          .join('T')
-          .split('/')
-          .join('-');
+  if ($ != undefined) {
+    if (carrier == 'JP') {
+      const tracking_data = [];
+      // Japan Post scrapping
+      let date_index = -9;
+      let date = '';
+      let status = '';
+      $('td').each((index, element) => {
+        const content = $(element).text();
+        if (content.indexOf('/') == 4) {
+          date_index = index;
+          date = content
+            .split(' ')
+            .join('T')
+            .split('/')
+            .join('-');
+        }
+        if (index - date_index == 1) {
+          status = content;
+        }
+        if (index - date_index == 4) {
+          tracking_data.push({
+            timestamp: date,
+            description: status,
+            location: content
+          });
+        }
+      });
+      // Try to acquire destination country
+      output['country'] = tracking_data[tracking_data.length - 1].location;
+      // Acquire last tracking update
+      output['status'] = tracking_data[tracking_data.length - 1].description;
+      // Acquire shipped date
+      output['shippeddate'] = tracking_data[0].timestamp.split('T')[0];
+      // Try to acquire delivered date
+      if (output['status'] == 'お届け済み') {
+        output['delivered'] = tracking_data[tracking_data.length - 1].timestamp.split('T')[0];
+      } else {
+        output['delivered'] = '';
       }
-      if (index - date_index == 1) {
-        status = content;
-      }
-      if (index - date_index == 4) {
-        tracking_data.push({
-          timestamp: date,
-          description: status,
-          location: content
-        });
-      }
-    });
-    // Try to acquire destination country
-    output['country'] = tracking_data[tracking_data.length - 1].location;
-    // Acquire last tracking update
-    output['status'] = tracking_data[tracking_data.length - 1].description;
-    // Acquire shipped date
-    output['shippeddate'] = tracking_data[0].timestamp.split('T')[0];
-    // Try to acquire delivered date
-    if (output['status'] == 'お届け済み') {
-      output['delivered'] = tracking_data[tracking_data.length - 1].timestamp.split('T')[0];
+      // Save raw data
+      output['rawdata'] = JSON.stringify({ shipments: [{ events: tracking_data }] });
     } else {
-      output['delivered'] = '';
+      // sample: https://www.dhl.com/cgi-bin/tracking.pl?AWB=1222865884
+      // DHL scrapping
+      // Try to acquire destination country
+      // Acquire last tracking update
+      // Acquire shipped date
+      // Try to acquire delivered date
+      // Save raw data
     }
-    // Save raw data
-    output['rawdata'] = JSON.stringify({ shipments: [{ events: tracking_data }] });
-  } else {
-    // sample: https://www.dhl.com/cgi-bin/tracking.pl?AWB=1222865884
-    // DHL scrapping
-    // Try to acquire destination country
-    // Acquire last tracking update
-    // Acquire shipped date
-    // Try to acquire delivered date
-    // Save raw data
   }
 
   return output;
--- tracker.js
+++ tracker.js
@@ -1,15 +1,40 @@
 const axios = require('axios');
 
 const fetchData = async siteUrl => {
-  const result = await axios.get(siteUrl, {
-    headers: {
-      accept: 'application/json',
-      'DHL-API-Key': process.env.DHL_API_KEY
-    }
-  });
-  result.data['HTML_status'] = result.status;
-  result.data['HTML_statusText'] = result.statusText;
-  return result.data;
+  let data = {};
+  await axios
+    .get(siteUrl, {
+      headers: {
+        accept: 'application/json',
+        'DHL-API-Key': process.env.DHL_API_KEY
+      }
+    })
+    .then(response => {
+      response.data['error'] = false;
+      response.data['HTML_status'] = response.status;
+      response.data['HTML_statusText'] = response.statusText;
+      data = response.data;
+    })
+    .catch(error => {
+      const data = { error: true, HTML_status: '', HTML_statusText: '' };
+      if (error.response == undefined) {
+        data.HTML_status = error.errno;
+        data.HTML_statusText = error.errno;
+      } else {
+        data.HTML_status = error.response.status;
+        data.HTML_statusText = error.response.statusText;
+      }
+    });
+  return data;
+  // const result = await axios.get(siteUrl, {
+  //   headers: {
+  //     accept: 'application/json',
+  //     'DHL-API-Key': process.env.DHL_API_KEY
+  //   }
+  // });
+  // result.data['HTML_status'] = result.status;
+  // result.data['HTML_statusText'] = result.statusText;
+  // return result.data;
 };
 
 const getResultsAPI = async (siteUrl, carrier) => {
@@ -19,40 +44,42 @@ const getResultsAPI = async (siteUrl, carrier) => {
     HTML_statusText: data.HTML_statusText
   };
 
-  if (carrier == 'JP') {
-    // Japan Post tracking
-    // Try to acquire destination country
-    // Acquire last tracking update
-    // Acquire shipped date
-    // Try to acquire delivered date
-    // Save raw data
-  } else if (carrier == 'USPS') {
-    // USPS tracking
-    // Try to acquire destination country
-    // Acquire last tracking update
-    // Acquire shipped date
-    // Try to acquire delivered date
-    // Save raw data
-  } else {
-    // DHL tracking
-    // Try to acquire destination country
-    const ad = data.shipments[0].destination.address.addressLocality.split(' - ');
-    output['country'] = ad[ad.length - 1];
-    // Acquire last tracking update
-    output['status'] = data.shipments[0].status.statusCode;
-    // Acquire shipped date
-    data.shipments[0].events.forEach(event => {
-      if (event.description == 'Shipment picked up') {
-        output['shippeddate'] = event.timestamp.split('T')[0];
+  if (data.error == false) {
+    if (carrier == 'JP') {
+      // Japan Post tracking
+      // Try to acquire destination country
+      // Acquire last tracking update
+      // Acquire shipped date
+      // Try to acquire delivered date
+      // Save raw data
+    } else if (carrier == 'USPS') {
+      // USPS tracking
+      // Try to acquire destination country
+      // Acquire last tracking update
+      // Acquire shipped date
+      // Try to acquire delivered date
+      // Save raw data
+    } else {
+      // DHL tracking
+      // Try to acquire destination country
+      const ad = data.shipments[0].destination.address.addressLocality.split(' - ');
+      output['country'] = ad[ad.length - 1];
+      // Acquire last tracking update
+      output['status'] = data.shipments[0].status.statusCode;
+      // Acquire shipped date
+      data.shipments[0].events.forEach(event => {
+        if (event.description == 'Shipment picked up') {
+          output['shippeddate'] = event.timestamp.split('T')[0];
+        }
+      });
+      // Try to acquire delivered date
+      output['delivered'] = '';
+      if (data.shipments[0].status.statusCode == 'delivered') {
+        output['delivered'] = data.shipments[0].status.timestamp.split('T')[0];
       }
-    });
-    // Try to acquire delivered date
-    output['delivered'] = '';
-    if (data.shipments[0].status.statusCode == 'delivered') {
-      output['delivered'] = data.shipments[0].status.timestamp.split('T')[0];
+      // Save raw data
+      output['rawdata'] = JSON.stringify(data);
     }
-    // Save raw data
-    output['rawdata'] = JSON.stringify(data);
   }
 
   return output;
