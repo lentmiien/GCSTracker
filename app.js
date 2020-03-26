c5f09bf0f34ae6bfa5756620ffac00be4837f249
レンナルト・ グランストロム <l_granstrom@intra.local>
2020-03-26 16:41:56 +0900

    Disable express request logging

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: false }));
 app.use(cookieParser());
