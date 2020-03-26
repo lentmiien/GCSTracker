8fc987f7c3033a873a0ef2db2be33e11bbdcf1d4
レンナルト・ グランストロム <l_granstrom@intra.local>
2020-03-27 08:59:29 +0900

    git push test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var logger = require('morgan');
 var session = require('express-session');
 
 const pp = require('./passport_init');
