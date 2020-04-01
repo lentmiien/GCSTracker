071987dacdd6066f58a40aec72a9d912327d8c4b
レンナルト・ グランストロム <l_granstrom@intra.local>
2020-04-01 14:31:51 +0900

    remove 0 values

--- public/javascripts/d3_trend_controller.js
+++ public/javascripts/d3_trend_controller.js
@@ -1,8 +1,8 @@
 const width = 500;
 const height = 250;
 const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-const data = JSON.parse(document.getElementById('chart_data').innerHTML);
 
+const data = JSON.parse(document.getElementById('chart_data').innerHTML);
 data.delivery_times.sort((a, b) => {
   if (a.date > b.date) {
     return 1;
@@ -12,6 +12,9 @@ data.delivery_times.sort((a, b) => {
     return 0;
   }
 });
+const dhl_data = data.delivery_times.filter(d => d.DHL_count > 0);
+const ems_data = data.delivery_times.filter(d => d.EMS_count > 0);
+const other_data = data.delivery_times.filter(d => d.OTHER_count > 0);
 
 // DHL
 let svg = d3
@@ -25,7 +28,7 @@ let svg = d3
 var x = d3
   .scaleTime()
   .domain(
-    d3.extent(data.delivery_times, function(d) {
+    d3.extent(dhl_data, function(d) {
       return d3.timeParse('%Y-%m-%d')(d.date);
     })
   )
@@ -39,7 +42,7 @@ var y = d3
   .scaleLinear()
   .domain([
     0,
-    d3.max(data.delivery_times, function(d) {
+    d3.max(dhl_data, function(d) {
       return d.DHL_count > 0 ? d.DHL_total_days / d.DHL_count : 0;
     })
   ])
@@ -48,7 +51,7 @@ svg.append('g').call(d3.axisLeft(y));
 
 svg
   .append('path')
-  .datum(data.delivery_times)
+  .datum(dhl_data)
   .attr('fill', 'none')
   .attr('stroke', 'orange')
   .attr('stroke-width', 1.5)
@@ -76,7 +79,7 @@ svg = d3
 var x = d3
   .scaleTime()
   .domain(
-    d3.extent(data.delivery_times, function(d) {
+    d3.extent(ems_data, function(d) {
       return d3.timeParse('%Y-%m-%d')(d.date);
     })
   )
@@ -90,7 +93,7 @@ var y = d3
   .scaleLinear()
   .domain([
     0,
-    d3.max(data.delivery_times, function(d) {
+    d3.max(ems_data, function(d) {
       return d.EMS_count > 0 ? d.EMS_total_days / d.EMS_count : 0;
     })
   ])
@@ -99,7 +102,7 @@ svg.append('g').call(d3.axisLeft(y));
 
 svg
   .append('path')
-  .datum(data.delivery_times)
+  .datum(ems_data)
   .attr('fill', 'none')
   .attr('stroke', 'green')
   .attr('stroke-width', 1.5)
@@ -127,7 +130,7 @@ svg = d3
 var x = d3
   .scaleTime()
   .domain(
-    d3.extent(data.delivery_times, function(d) {
+    d3.extent(other_data, function(d) {
       return d3.timeParse('%Y-%m-%d')(d.date);
     })
   )
@@ -141,7 +144,7 @@ var y = d3
   .scaleLinear()
   .domain([
     0,
-    d3.max(data.delivery_times, function(d) {
+    d3.max(other_data, function(d) {
       return d.OTHER_count > 0 ? d.OTHER_total_days / d.OTHER_count : 0;
     })
   ])
@@ -150,7 +153,7 @@ svg.append('g').call(d3.axisLeft(y));
 
 svg
   .append('path')
-  .datum(data.delivery_times)
+  .datum(other_data)
   .attr('fill', 'none')
   .attr('stroke', 'steelblue')
   .attr('stroke-width', 1.5)
