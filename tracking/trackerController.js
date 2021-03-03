20df45427d381caf2e21f026eb0800cab4a289aa
lentmiien <lentmiien@gmail.com>
2021-03-03 18:33:43 +0900

    USPS every 2 days

--- tracking/trackerController.js
+++ tracking/trackerController.js
@@ -147,7 +147,7 @@ async function TrackAll() {
               [Op.gte]: cutoff,
             },
             lastchecked: {
-              [Op.lte]: postalnc,
+              [Op.lte]: dhlnc,
             },
           },
         }).then((entry) => callback(null, entry));
